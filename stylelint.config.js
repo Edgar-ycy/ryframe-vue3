473a6338cb293ed162f1dc7e4bdfe8a11f74396c
edgar_ye <edgar_ye@petalmail.com>
2026-08-24 18:50:44 +0800

    fix(frontend): 允许外置作用域样式使用深度选择器

--- stylelint.config.js
+++ stylelint.config.js
@@ -8,6 +8,12 @@ export default {
     'custom-property-pattern': null,
     'keyframes-name-pattern': null,
     'selector-class-pattern': null,
+    'selector-pseudo-class-no-unknown': [
+      true,
+      {
+        ignorePseudoClasses: ['deep', 'global'],
+      },
+    ],
     'scss/dollar-variable-pattern': null,
   },
 }
